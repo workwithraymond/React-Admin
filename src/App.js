2b477f5a083acb870ef49b5737813242c574dddf
WorkWithRay <workwithraymond01@gmail.com>
2022-12-20 17:31:27 -0500

    new change

--- src/App.js
+++ src/App.js
@@ -16,21 +16,17 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Calendar from "./scenes/calendar/calendar";
 
-
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
 
   return (
-    
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          
           <Sidebar isSidebar={isSidebar} />
           <main className="content">
-            
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard />} />
